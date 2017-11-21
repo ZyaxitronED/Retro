4fa9ebead9c96a1e1e6a4f7f951e5f80f8ddbb8b
ZyaxitronED <33060742+ZyaxitronED@users.noreply.github.com>
2017-11-21 13:09:55 +0000

    added image and assets

---
+++ sketch.js
@@ -0,0 +1,224 @@
+function setup(){
+  createCanvas(400, 400);
+  noLoop();
+  //stroke(0);
+}
+
+function draw(){
+  background(255);
+
+  fill(255);
+  noStroke();
+
+  //Row 1
+  rect(0, 0, 40, 40);
+  rect(40, 0, 40, 40);
+  rect(80, 0, 40, 40);
+  rect(120, 0, 40, 40);
+  rect(160, 0, 40, 40);
+  rect(200, 0, 40, 40);
+  rect(240, 0, 40, 40);
+  rect(280, 0, 40, 40);
+  rect(320, 0, 40, 40);
+  rect(360, 0, 40, 40);
+
+  //Row 2
+  rect(0, 40, 40, 40);
+  rect(40, 40, 40, 40);
+  rect(80, 40, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(120, 40, 40, 40);
+  rect(160, 40, 40, 40);
+  rect(200, 40, 40, 40);
+  rect(240, 40, 40, 40);
+  var c = color(255, 255, 255);
+  fill(c);
+  rect(280, 40, 40, 40);
+  rect(320, 40, 40, 40);
+  rect(360, 40, 40, 40);
+
+  //Row 3
+  rect(0, 80, 40, 40);
+  rect(40, 80, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(80, 80, 40, 40);
+  var c = color(130, 130, 130);
+  fill(c);
+  rect(120, 80, 40, 40);
+  rect(160, 80, 40, 40);
+  rect(200, 80, 40, 40);
+  rect(240, 80, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(280, 80, 40, 40);
+  var c = color(255, 255, 255);
+  fill(c);
+  rect(320, 80, 40, 40);
+  rect(360, 80, 40, 40);
+
+  //Row 4
+  rect(0, 120, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(40, 120, 40, 40);
+  var c = color(130, 130, 130);
+  fill(c);
+  rect(80, 120, 40, 40);
+  rect(120, 120, 40, 40);
+  rect(160, 120, 40, 40);
+  rect(200, 120, 40, 40);
+  rect(240, 120, 40, 40);
+  rect(280, 120, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(320, 120, 40, 40);
+  var c = color(255, 255, 255);
+  fill(c);
+  rect(360, 120, 40, 40);
+
+  //Row 5
+  rect(0, 160, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(40, 160, 40, 40);
+  var c = color(130, 130, 130);
+  fill(c);
+  rect(80, 160, 40, 40);
+  rect(120, 160, 40, 40);
+  rect(160, 160, 40, 40);
+  rect(200, 160, 40, 40);
+  rect(240, 160, 40, 40);
+  rect(280, 160, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(320, 160, 40, 40);
+  var c = color(255, 255, 255);
+  fill(c);
+  rect(360, 160, 40, 40);
+
+  //Row 6
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(0, 200, 40, 40);
+  var c = color(255, 148, 148);
+  fill(c);
+  rect(40, 200, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(80, 200, 40, 40);
+  var c = color(130, 130, 130);
+  fill(c);
+  rect(120, 200, 40, 40);
+  var c = color(200, 200, 200);
+  fill(c);
+  rect(160, 200, 40, 40);
+  rect(200, 200, 40, 40);
+  var c = color(130, 130, 130);
+  fill(c);
+  rect(240, 200, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(280, 200, 40, 40);
+  var c = color(255, 148, 148);
+  fill(c);
+  rect(320, 200, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(360, 200, 40, 40);
+  var c = color(255, 255, 255);
+  fill(c);
+
+  //Row 7
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(0, 240, 40, 40);
+  var c = color(255, 148, 148);
+  fill(c);
+  rect(40, 240, 40, 40);
+  rect(80, 240, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(120, 240, 40, 40);
+  rect(160, 240, 40, 40);
+  rect(200, 240, 40, 40);
+  rect(240, 240, 40, 40);
+  var c = color(255, 148, 148);
+  fill(c);
+  rect(280, 240, 40, 40);
+  rect(320, 240, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(360, 240, 40, 40);
+  var c = color(255, 255, 255);
+  fill(c);
+
+  //Row 8
+  rect(0, 280, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(40, 280, 40, 40);
+  var c = color(255, 148, 148);
+  fill(c);
+  rect(80, 280, 40, 40);
+  var c = color(60, 190, 250);
+  fill(c);
+  rect(120, 280, 40, 40);
+  var c = color(255, 148, 148);
+  fill(c);
+  rect(160, 280, 40, 40);
+  rect(200, 280, 40, 40);
+  var c = color(60, 190, 250);
+  fill(c);
+  rect(240, 280, 40, 40);
+  var c = color(255, 148, 148);
+  fill(c);
+  rect(280, 280, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(320, 280, 40, 40);
+  var c = color(255, 255, 255);
+  fill(c);
+  rect(360, 280, 40, 40);
+
+  //Row 9
+  rect(0, 320, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(40, 320, 40, 40);
+  var c = color(255, 148, 148);
+  fill(c);
+  rect(80, 320, 40, 40);
+  rect(120, 320, 40, 40);
+  var c = color(255, 80, 80);
+  fill(c);
+  rect(160, 320, 40, 40);
+  rect(200, 320, 40, 40);
+  var c = color(255, 148, 148);
+  fill(c);
+  rect(240, 320, 40, 40);
+  rect(280, 320, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(320, 320, 40, 40);
+  var c = color(255, 255, 255);
+  fill(c);
+  rect(360, 320, 40, 40);
+
+  //Row 10
+  rect(0, 360, 40, 40);
+  rect(40, 360, 40, 40);
+  var c = color(0, 0, 0);
+  fill(c);
+  rect(80, 360, 40, 40);
+  rect(120, 360, 40, 40);
+  rect(160, 360, 40, 40);
+  rect(200, 360, 40, 40);
+  rect(240, 360, 40, 40);
+  rect(280, 360, 40, 40);
+  var c = color(255, 255, 255);
+  fill(c);
+  rect(320, 360, 40, 40);
+  rect(360, 360, 40, 40);
+}
